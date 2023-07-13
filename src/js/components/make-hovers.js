e9f04adfaaa141b596d69ae3d336d10c02af21f8
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-07-13 16:09:08 +0500

    done?

---
+++ src/js/components/make-hovers.js
@@ -0,0 +1,41 @@
+const names = document.querySelectorAll(".hero__text-name");
+const phones = document.querySelectorAll(".hero__text-phone");
+const jobs = document.querySelectorAll(".hero__text-job");
+const emails = document.querySelectorAll(".hero__text-email");
+const salaries = document.querySelectorAll(".hero__text-salary");
+const cities = document.querySelectorAll(".hero__text-city");
+const links = document.querySelectorAll(".hero__text-link");
+const crms = document.querySelectorAll(".hero__text-crm");
+
+const arrays = [names, phones, jobs, emails, salaries, cities, links, crms];
+
+for (let i = 0; i < arrays.length; i++) {
+	arrays[i].forEach((hoverElement) => {
+		hoverElement.onmouseenter = () => {
+			if (i !== arrays.length - 1) {
+				arrays[i].forEach((el) => {
+					el.classList.add("hero__text-visible");
+				});
+			}
+			if (i !== 0) {
+				arrays[i - 1].forEach((el) => {
+					el.classList.add("hero__text-visible");
+				});
+			}
+		};
+		hoverElement.onmouseleave = () => {
+			if (i !== arrays.length - 1) {
+				arrays[i].forEach((el) => {
+					el.classList.remove("hero__text-visible");
+				});
+			}
+			if (i !== 0) {
+				arrays[i - 1].forEach((el) => {
+					el.classList.remove("hero__text-visible");
+				});
+			}
+		};
+		console.log(i, arrays.length - i);
+	});
+	console.log(arrays[i]);
+}
